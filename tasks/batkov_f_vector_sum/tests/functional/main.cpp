0f5e4f781d08950629073ef93f0632f5999e0c4e
weyland0 <philippbatkov@gmail.com>
2025-11-19 12:54:03 +0300

    add func tests

---
+++ tasks/batkov_f_vector_sum/tests/functional/main.cpp
@@ -0,0 +1,102 @@
+#include <gtest/gtest.h>
+#include <stb/stb_image.h>
+
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <numeric>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include "batkov_f_vector_sum/common/include/common.hpp"
+#include "batkov_f_vector_sum/mpi/include/ops_mpi.hpp"
+#include "batkov_f_vector_sum/seq/include/ops_seq.hpp"
+#include "util/include/func_test_util.hpp"
+#include "util/include/util.hpp"
+
+namespace batkov_f_vector_sum {
+
+class BatkovFRunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+ public:
+  static std::string PrintTestParam(const TestType &test_param) {
+
+    std::string filename = std::get<0>(test_param);
+    int expected_sum = std::get<1>(test_param);
+
+    std::string sanitized_filename = SanitizeParamName(filename);
+
+    return sanitized_filename + "_" + std::to_string(expected_sum);
+  }
+
+ protected:
+  void SetUp() override {
+    
+    TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
+    std::string filename = std::get<0>(params);
+    expected_sum_ = std::get<1>(params);
+
+    std::string abs_path = ppc::util::GetAbsoluteTaskPath(PPC_ID_batkov_f_vector_sum, filename);
+    std::ifstream file(abs_path);
+
+    if (!file.is_open()) {
+      throw std::runtime_error("Failed to open file: " + abs_path);
+    }
+
+    int value;
+    while (file >> value) {
+      input_data_.push_back(value);
+    }
+    file.close();
+
+  }
+
+  bool CheckTestOutputData(OutType &output_data) final {
+    return (expected_sum_ == output_data);
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+
+  static std::string SanitizeParamName(const std::string &name) {
+    std::string sanitized = name;
+    for (char &c : sanitized) {
+      if (!std::isalnum(static_cast<unsigned char>(c)) && c != '_') {
+        c = '_';
+      }
+    }
+
+    return sanitized;
+  }
+
+ private:
+  InType input_data_;
+  OutType expected_sum_ = 0;
+};
+
+namespace {
+
+TEST_P(BatkovFRunFuncTestsProcesses, MatmulFromPic) {
+  ExecuteTest(GetParam());
+}
+
+const std::array<TestType, 2> kTestParam = {std::make_tuple("vec1.txt", 5050),
+                                            std::make_tuple("vec2.txt", 1450) };
+
+const auto kTestTasksList =
+    std::tuple_cat(ppc::util::AddFuncTask<BatkovFVectorSumMPI, InType>(kTestParam, PPC_SETTINGS_batkov_f_vector_sum),
+                   ppc::util::AddFuncTask<BatkovFVectorSumSEQ, InType>(kTestParam, PPC_SETTINGS_batkov_f_vector_sum));
+
+const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
+
+const auto kPerfTestName = BatkovFRunFuncTestsProcesses::PrintFuncTestName<BatkovFRunFuncTestsProcesses>;
+
+INSTANTIATE_TEST_SUITE_P(VectorSumFuncTests, BatkovFRunFuncTestsProcesses, kGtestValues, kPerfTestName);
+
+}  // namespace
+
+}  // namespace batkov_f_vector_sum
