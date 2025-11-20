aa466f828214e84ae8e8f6c3dfd06fb097818e96
weyland0 <philippbatkov@gmail.com>
2025-11-20 12:28:02 +0300

    increase the size of the test data

--- tasks/batkov_f_vector_sum/tests/performance/main.cpp
+++ tasks/batkov_f_vector_sum/tests/performance/main.cpp
@@ -27,6 +27,12 @@ class BatkovFRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
       input_data_.push_back(value);
     }
 
+    for (size_t i = 0; i < 7; ++i) {
+      input_data_.insert(input_data_.end(), input_data_.begin(), input_data_.end());
+
+    }
+    expected_sum_ *= 128;
+
     file.close();
   }
 
