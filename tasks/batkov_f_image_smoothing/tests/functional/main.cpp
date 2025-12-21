24f124aba361a52f66139d1c09e920b87404566c
weyland0 <philippbatkov@gmail.com>
2025-12-21 19:44:03 +0300

    update functional/main.cpp

--- tasks/batkov_f_image_smoothing/tests/functional/main.cpp
+++ tasks/batkov_f_image_smoothing/tests/functional/main.cpp
@@ -119,8 +119,8 @@ TEST_P(BatkovFRunFuncTestsProcesses2, ImageSmoothing) {
 }
 
 const std::array<TestType, 4> kTestParam = {
-    std::make_tuple("small_image", 30, 30, 3), std::make_tuple("medium_image", 150, 150, 3),
-    std::make_tuple("big_image", 300, 300, 3), std::make_tuple("large_image", 500, 500, 3)};
+    std::make_tuple("tiny_image", 10, 10, 3), std::make_tuple("small_image", 50, 50, 3),
+    std::make_tuple("medium_image", 150, 150, 3), std::make_tuple("big_image", 300, 300, 3)};
 
 const auto kTestTasksList = std::tuple_cat(
     ppc::util::AddFuncTask<BatkovFImageSmoothingSEQ, InType>(kTestParam, PPC_SETTINGS_batkov_f_image_smoothing),
